76636823e3844f5cd4358127c40d1be8f9476989
kanika <kanika.midha@students.iiit.ac.in>
2017-09-09 07:33:19 +0530

    "Adding documentation and Socket.h"

--- Socket.h
+++ Socket.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <unistd.h>
+#include <ctime>
 
 class Socket {
 protected:
   Socket() = default;
   int m_fd;
+  time_t m_read_timeout;
+  time_t m_write_timeout;
 public:
   Socket(const Socket &) = delete;
   int fd() const { return m_fd; }
@@ -13,13 +16,29 @@ public:
   enum class HandlerResult {
     NORMAL,
     REMOVE,
-    NORMAL_LISTEN,
+    NORMAL_LISTEN,        //This is returned by listener sockets to make a distinction b/w listener socket and connection sockets
+    NORMAL_CONTINUE,      //This is returned in case of Partial Message read See MessageRecieveSocket.cpp HandleRead() for more details.
   };
 
-  // Returning "false" means please remove me
   virtual HandlerResult HandleRead() = 0;
   virtual HandlerResult HandleWrite() = 0;
   virtual bool HasDataForWriting() = 0;
+  time_t getReadTimeout()
+  {
+    return m_read_timeout;
+  }
+  time_t getWriteTimeout()
+  {
+    return m_write_timeout;
+  }
+  void setReadTimeout(time_t read_timeout)
+  {
+    m_read_timeout=read_timeout;
+  }
+  void setWriteTimeout(time_t write_timeout)
+  {
+    m_write_timeout=write_timeout;
+  }
   virtual ~Socket() {
     close(m_fd);
   }
